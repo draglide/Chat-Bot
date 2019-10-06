864ceb06666f3f3abc2e6d9ebf62d43a40f921b7
Adi <darkzzangelzz@gmail.com>
2019-10-06 12:34:10 +0700

    init

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,7 +8,7 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 require("jquery")
-require("popper")
+require("popper.js")
 require("bootstrap/dist/js/bootstrap")
 
 // Uncomment to copy all static images under ../images to the output folder and reference
